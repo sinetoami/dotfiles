2bf9c39a57eec9053f6ff8cef876946a54f3f110
Sinésio Neto <sn.logbackup@gmail.com>
2019-07-25 04:20:33 -0300

    Update .bin and aliases sources

--- zsh/.zsh/plugins/git/git.plugin.zsh
+++ zsh/.zsh/plugins/git/git.plugin.zsh
@@ -2,25 +2,25 @@
 alias ggc='git-config-template' # [g]it [g]enerate [c]onfig
 
 # git branch
-alias gbli='git-fzy-branch branch --list' # [g]it [b]ranch [li]st
-alias gba='git-fzy-branch branch --all' # [g]it [b]ranch [a]ll
-alias gbr='git-fzy-branch branch --remote' # [g]it [b]ranch [r]emote
-alias gbd='git-fzy-branch branch --all -d' # [g]it [b]ranch [d]elete
-alias gbD='git-fzy-branch branch --all -D' # [g]it [b]ranch [d]elete
-alias gbdm='git-fzf-branch branch --all -d' # [g]it [b]ranch [d]elete [m]ulti
-alias gbDm='git-fzf-branch branch --all -D' # [g]it [b]ranch [d]elete [m]ulti
-alias gbap='git-fzf-branch branch --all' # [g]it [b]ranch [d]elete [m]ulti
+alias gbli='git-branch-fzy branch --list' # [g]it [b]ranch [li]st
+alias gba='git-branch-fzy branch --all' # [g]it [b]ranch [a]ll
+alias gbr='git-branch-fzy branch --remote' # [g]it [b]ranch [r]emote
+alias gbd='git-branch-fzy branch --all -d' # [g]it [b]ranch [d]elete
+alias gbD='git-branch-fzy branch --all -D' # [g]it [b]ranch [d]elete
+alias gbdm='git-branch-fzf branch --all -d' # [g]it [b]ranch [d]elete [m]ulti
+alias gbDm='git-branch-fzf branch --all -D' # [g]it [b]ranch [d]elete [m]ulti
+alias gbap='git-branch-fzf branch --all' # [g]it [b]ranch [d]elete [m]ulti
 
 # git checkout
 alias gcb='git checkout -b' # [g]it [c]heckout -[b]
-alias gco='git-fzy-branch checkout --all' # [g]it [c]heck[o]ut
-alias gcop='git-fzf-branch checkout --all' # [g]it [c]heck[o]ut with [p]review 
+alias gco='git-branch-fzy checkout --all' # [g]it [c]heck[o]ut
+alias gcop='git-branch-fzf checkout --all' # [g]it [c]heck[o]ut with [p]review 
 
 # git tag
-alias gtl='git-fzy-branch tag --list' # [g]it [t]ag [l]ist
-alias gtd='git-fzy-branch tag --all -d' # [g]it [t]ag [l]ist
-alias gtlp='git-fzf-branch tag --list' # [g]it [t]ag [l]ist with [p]review
-alias gtdm='git-fzf-branch tag --all -d' # [g]it [t]ag [d]elete [m]ulti
+alias gtl='git-branch-fzy tag --list' # [g]it [t]ag [l]ist
+alias gtd='git-branch-fzy tag --all -d' # [g]it [t]ag [l]ist
+alias gtlp='git-branch-fzf tag --list' # [g]it [t]ag [l]ist with [p]review
+alias gtdm='git-branch-fzf tag --all -d' # [g]it [t]ag [d]elete [m]ulti
 
 # git blame
 alias gbl='git blame' # [g]it [bl]ame
